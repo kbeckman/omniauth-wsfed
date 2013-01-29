bc4ee41042e68202ec2aa741bae68f86a73a3170
kbeckman <kbeckman.c4sc@gmail.com>
2013-01-28 21:18:17 -0600

    adding validation call to callback_phase

--- lib/omniauth/strategies/wsfed.rb
+++ lib/omniauth/strategies/wsfed.rb
@@ -6,10 +6,11 @@ module OmniAuth
     class WSFed
       include OmniAuth::Strategy
 
-      autoload :AuthRequest,      'omniauth/strategies/wsfed/auth_request'
-      autoload :AuthResponse,     'omniauth/strategies/wsfed/auth_response'
-      autoload :ValidationError,  'omniauth/strategies/wsfed/validation_error'
-      autoload :XMLSecurity,      'omniauth/strategies/wsfed/xml_security'
+      autoload :AuthRequest,        'omniauth/strategies/wsfed/auth_request'
+      autoload :AuthResponse,       'omniauth/strategies/wsfed/auth_response'
+      autoload :CallbackValidator,  'omniauth/strategies/wsfed/callback_validator'
+      autoload :ValidationError,    'omniauth/strategies/wsfed/validation_error'
+      autoload :XMLSecurity,        'omniauth/strategies/wsfed/xml_security'
 
       # Issues passive WS-Federation redirect for authentication...
       def request_phase
@@ -30,16 +31,22 @@ module OmniAuth
       # Parse SAML token...
       def callback_phase
         begin
-          response = OmniAuth::Strategies::WSFed::AuthResponse.new(request.params['wresult'], options)
+          response  = OmniAuth::Strategies::WSFed::AuthResponse.new(request.params['wresult'], options)
+          validator = OmniAuth::Strategies::WSFed::CallbackValidator.new(response, options)
+
+          validator.validate!
 
           @name_id  = response.name_id
           @claims   = response.attributes
 
-          return fail!(:invalid_ticket, OmniAuth::Strategies::WSFed::ValidationError('Invalid SAML Token') ) if @claims.nil? || @claims.empty? || !response.valid?
           super
+
         rescue ArgumentError => e
-          fail!(:invalid_ticket, 'Invalid WSFed Response')
+          fail!(:invalid_response, e)
+        rescue OmniAuth::Strategies::WSFed::ValidationError => e
+          fail!(:invalid_authn_token, e)
         end
+
       end
 
       # OmniAuth DSL methods...
