6c0b02d380da98e7008093d83901b2d9c0c80d8f
kbeckman <kbeckman.c4sc@gmail.com>
2013-02-22 13:42:29 -0600

    refactoring callback_validator attribute names

--- lib/omniauth/strategies/wsfed/callback_validator.rb
+++ lib/omniauth/strategies/wsfed/callback_validator.rb
@@ -4,7 +4,7 @@ module OmniAuth
 
       class CallbackValidator
 
-        attr_accessor :auth_response, :wsfed_settings
+        attr_accessor :auth_callback, :wsfed_settings
 
         ISSUER_MISMATCH     = 'AuthN token issuer does not match configured issuer.'
         AUDIENCE_MISMATCH   = 'AuthN token audience does not match configured realm.'
@@ -13,29 +13,29 @@ module OmniAuth
         NO_CLAIMS           = 'AuthN token contains no claims.'
         NO_USER_IDENTIFIER  = 'AuthN token contains no user identifier. Verify that configured :id_claim setting is correct.'
 
-        def initialize(auth_response, wsfed_settings)
-          self.auth_response  = auth_response
+        def initialize(auth_callback, wsfed_settings)
+          self.auth_callback  = auth_callback
           self.wsfed_settings = wsfed_settings
         end
 
         def validate!
           raise OmniAuth::Strategies::WSFed::ValidationError.new(ISSUER_MISMATCH) unless
-            auth_response.issuer == wsfed_settings[:issuer_name]
+            auth_callback.issuer == wsfed_settings[:issuer_name]
 
           raise OmniAuth::Strategies::WSFed::ValidationError.new(AUDIENCE_MISMATCH) unless
-              auth_response.audience == wsfed_settings[:realm]
+            auth_callback.audience == wsfed_settings[:realm]
 
           raise OmniAuth::Strategies::WSFed::ValidationError.new(FUTURE_CREATED_AT) unless
-              auth_response.created_at < Time.now.utc
+            auth_callback.created_at < Time.now.utc
 
           raise OmniAuth::Strategies::WSFed::ValidationError.new(TOKEN_EXPIRED) unless
-              auth_response.expires_at > Time.now.utc
+            auth_callback.expires_at > Time.now.utc
 
-          if auth_response.claims.nil? || auth_response.claims.empty?
+          if auth_callback.claims.nil? || auth_callback.claims.empty?
             raise OmniAuth::Strategies::WSFed::ValidationError.new(NO_CLAIMS)
           end
 
-          if auth_response.name_id.nil? || auth_response.name_id.empty?
+          if auth_callback.name_id.nil? || auth_callback.name_id.empty?
             raise OmniAuth::Strategies::WSFed::ValidationError.new(NO_USER_IDENTIFIER)
           end
 
